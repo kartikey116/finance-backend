1cc8c76f5f093ea17cd208b8ea29bcd2143e21d6
KARTIKEY <kartikeyu07@gmail.com>
2026-04-05 23:53:49 +0530

    Update app.js to add health check routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,13 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 import { errorHandler } from "./middleware/error.middleware.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
-
-// Routes
 import authRoutes from "./modules/auth/auth.routes.js";
 import userRoutes from "./modules/user/user.routes.js";
 import financeRoutes from "./modules/finance/finance.routes.js";
 import dashboardRoutes from "./modules/dashboard/dashboard.routes.js";
-
+import healthRoutes from "./modules/health/health.routes.js";
 import { setupSwagger } from "./config/swagger.js";
 
 const app = express();
@@ -23,19 +20,13 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(morgan("dev"));
 
-// Setup API Docs (Swagger)
 setupSwagger(app);
-
-// Global rate limiter
 app.use(rateLimiter);
-
-// Mount routes
+app.use("/api/health", healthRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/finance", financeRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-
-// Error middleware (must be at the end)
 app.use(errorHandler);
 
 export default app;
